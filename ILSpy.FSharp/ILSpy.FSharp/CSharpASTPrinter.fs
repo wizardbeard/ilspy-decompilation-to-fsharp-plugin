4a870ac38d2b7e0ddd954e8eb3cd570b17d386fe
rsdpisuy <rsdpisuy@gmail.com>
2012-12-11 09:59:44 +0400

    More pretty wrapper.

--- ILSpy.FSharp/ILSpy.FSharp/CSharpASTPrinter.fs
+++ ILSpy.FSharp/ILSpy.FSharp/CSharpASTPrinter.fs
@@ -2,35 +2,16 @@
 
 open ICSharpCode.Decompiler
 open ICSharpCode.NRefactory.CSharp
+open ILSpy.FSahrp.PrinterWrapper
 
 type CSharpASTPrinter() =
     
     [<Literal>]
     let moduleName = "FS DECOMPILER"
 
-    let mutable output = Unchecked.defaultof<ITextOutput>
-
-    let wordL (txt:string) = output.Write txt 
-
-    let def txt n local = output.WriteDefinition(txt,n,local)
-
-    let aboveListL layoutF lst =
-        lst |> List.iter (fun s -> layoutF s; output.WriteLine())        
-
-    let tab layoutF = 
-        output.Indent()
-        layoutF()
-        output.Unindent()
-
-    let ws () = wordL " "
-
-    let brake layoutF =
-        output.WriteLine()
-        layoutF()
-
     let error msg = 
         "(*" + moduleName + ". " + msg + "*)"
-        |> wordL           
+        |> wordL
 
     let rec pwit (ast: AstNode) (output: ITextOutput) count =
         match count with
@@ -42,52 +23,43 @@ type CSharpASTPrinter() =
         ast.Children |> Seq.iter (fun child -> pwit child output (count + 1))
 
     let rec namespaceLayout (nmsp : NamespaceDeclaration) =        
-        let nameLayout () = wordL nmsp.Name
-        let bodyLayout () = nmsp.Members |> List.ofSeq |> aboveListL past
-        wordL "namespace"
-        ws()
-        nameLayout ()
-        brake (fun () -> tab bodyLayout)
+        let nameLayout = wordL nmsp.Name
+        let bodyLayout = nmsp.Members |> List.ofSeq |> List.map past |> aboveListL 
+        wordL "namespace" ++ nameLayout
+        @@-- bodyLayout
 
     and typeDeclLayout (tDecl:TypeDeclaration) =        
-        let nameLayout () = def tDecl.Name tDecl true
-        let bodyLayout () = tDecl.Members |> List.ofSeq |> aboveListL past
-        wordL "type"
-        ws()
-        nameLayout ()
-        wordL "() ="
-        brake (fun () -> tab bodyLayout)
+        let nameLayout = defL tDecl.Name tDecl true
+        let bodyLayout = tDecl.Members |> List.ofSeq |> List.map past |> aboveListL
+        wordL "type" ++ nameLayout ++ wordL "() ="
+        @@-- bodyLayout
 
     and propDeclLayout (pDecl:PropertyDeclaration) =
-        let nameLayout () = wordL ("this." + pDecl.Name)
-        let bodyLayout () = past pDecl.Getter
-        wordL "member"
-        ws()
-        nameLayout ()
-        brake (fun () -> tab bodyLayout)
+        let nameLayout = wordL ("this." + pDecl.Name)
+        let bodyLayout = past pDecl.Getter
+        wordL "member" ++ nameLayout
+        @@-- bodyLayout
 
     and accessorLayout (acs:Accessor) =        
-        let bodyLayout () = past acs.Body
+        let bodyLayout = past acs.Body
         wordL "with get() ="
-        brake (fun () -> tab bodyLayout)
+        @@-- bodyLayout
 
     and uDeclLayout (uDecl:UsingDeclaration) =        
-        let nameLayout () = past uDecl.Import
-        wordL "open" 
-        ws()
-        nameLayout ()
+        let nameLayout = past uDecl.Import
+        wordL "open" ++ nameLayout
 
     and past (ast: AstNode) =
         let astChildrenCashed = ast.Children |> List.ofSeq
         match ast with
-        | :? SyntaxTree -> astChildrenCashed |> aboveListL (fun child -> past child)
+        | :? SyntaxTree -> astChildrenCashed |> List.map past |> aboveListL
         | :? NamespaceDeclaration as nmsp -> namespaceLayout nmsp
         | :? Identifier as id -> wordL id.Name
         | :? TypeDeclaration as typeDecl -> typeDeclLayout typeDecl
         | :? UsingDeclaration as uDecl -> uDeclLayout uDecl
         | :? PropertyDeclaration as pDecl -> propDeclLayout pDecl
         | :? Accessor as acs -> accessorLayout acs
-        | :? BlockStatement -> astChildrenCashed |> aboveListL (fun child -> past child)
+        | :? BlockStatement -> astChildrenCashed |> List.map past |> aboveListL
         | :? ReturnStatement -> past ast.FirstChild
         | :? PrimitiveExpression -> ast.ToString() |> wordL
         | x -> "Node is not supported: " + string x |> error
@@ -95,7 +67,7 @@ type CSharpASTPrinter() =
     member this.PrintWhatIsThere(ast: AstNode, output: ITextOutput) =
         pwit ast output 0
 
-    member this.PrintAST(ast: AstNode, _output: ITextOutput) =
-        output <- _output
+    member this.PrintAST(ast: AstNode, output: ITextOutput) =        
         past ast
+        |> print output
 
---
+++ ILSpy.FSharp/ILSpy.FSharp/PrinterWrapper.fs
@@ -0,0 +1,27 @@
+﻿module ILSpy.FSahrp.PrinterWrapper
+
+open ICSharpCode.Decompiler
+
+let inline (@@--) l r = 
+    fun (output:ITextOutput) ->
+        l output
+        output.WriteLine()
+        output.Indent()
+        r output
+        output.Unindent() 
+
+let inline (++) l r = 
+    fun (output:ITextOutput) ->
+        l output
+        output.Write(" ") 
+        r output
+
+let wordL (txt:string) = fun (output:ITextOutput) -> output.Write txt 
+
+let defL txt n local = fun (output:ITextOutput) -> output.WriteDefinition(txt,n,local)
+
+let aboveListL lst =
+    fun (output:ITextOutput) -> lst |> List.iter (fun layoutF -> layoutF output; output.WriteLine())
+
+let print (output:ITextOutput) layoutF = layoutF output
+
